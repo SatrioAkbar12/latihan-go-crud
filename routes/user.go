a43a40a509f0a4f44d6efa512a249fbeecbc311d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-03-22 21:11:38 +0700

    add user route

---
+++ routes/user.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/SatrioAkbar12/latihan-go-crud/controllers"
+	"github.com/labstack/echo"
+)
+
+func UserRoutes(e echo.Echo) {
+	e.GET("/user", controllers.GetUsers)
+	e.POST("/user", controllers.CreateUser)
+	e.GET("/user/:id", controllers.GetUserByID)
+	e.PUT("/user/:id", controllers.UpdateUser)
+	e.DELETE("user/:id", controllers.DeleteUser)
+}
