e605faeb63e5ef41923de9d3efb98d3c98de08f5
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2023-03-23 17:46:12 +0700

    add validator on initial routing

--- routes/initial.go
+++ routes/initial.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	"github.com/SatrioAkbar12/latihan-go-crud/models"
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func RouteInitial() {
 	e := echo.New()
+	e.Validator = &models.CustomValidator{Validator: validator.New()}
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
